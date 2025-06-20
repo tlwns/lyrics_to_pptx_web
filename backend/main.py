9b9e2b76ff73e0f5974c535e19097ed786e48990
Sijun Lee <sijun.lee03@gmail.com>
2025-06-20 20:46:52 +1000

    wip: added fastapi CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, responses
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for simplicity
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 @app.post("/generate")
 async def generate_pptx(lyrics: str, filename: str = ""):
     try:
-        pass
+        file_path = "generated_presentation.pptx"
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -17,3 +26,9 @@ async def generate_pptx(lyrics: str, filename: str = ""):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An unexpected error occurred: " + str(e)
         ) from e
+
+    return responses.FileResponse(
+        path=file_path,
+        filename=filename if filename else "default.pptx",
+        media_type="application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    )
