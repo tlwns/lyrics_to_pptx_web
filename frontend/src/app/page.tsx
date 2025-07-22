960175da13a2fec7a4d2b7ce461d8d7abbb08074
Sijun Lee <sijun.lee03@gmail.com>
2025-07-22 23:23:14 +1000

    feat: enhance layout and styling of the home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,17 @@
 import LyricsForm from '@/app/ui/lyrics-form';
+import { Music } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main
-      className='flex flex-col items-center justify-center min-h-screen bg-neutral-50
-'
-    >
-      <div className='w-full max-w-md px-6 py-6 rounded-lg shadow-lg border-gray-900/10 border bg-white'>
+    <main className='flex flex-col items-center justify-center min-h-screen md:bg-indigo-600'>
+      <div className='mb-10 mt-6 flex items-center gap-2'>
+        <h2 className='text-center text-2xl/9 font-bold tracking-tight md:text-white text-indigo-600 '>
+          Lyrics to PPTX Generator
+        </h2>
+        <Music className='text-indigo-600 md:text-white' />
+      </div>
+
+      <div className='w-full max-w-md px-6 py-6 rounded-lg md:shadow-lg border-gray-900/10 md:border bg-white'>
         <LyricsForm />
       </div>
     </main>
