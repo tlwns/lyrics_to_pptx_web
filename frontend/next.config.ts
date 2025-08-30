416bb732b97224d1cb5fc1fea68539aaf8c83777
Sijun Lee <sijun.lee03@gmail.com>
2025-08-30 16:46:29 +1000

    feat: change server actions body size limit due to bigger pptx file sizes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '3mb',
+    },
+  },
 };
 
 export default nextConfig;
